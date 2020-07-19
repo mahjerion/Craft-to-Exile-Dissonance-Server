26118c0c7a41b0291964e3e6f48894790aa0ec9b
mahjerion <66142256+mahjerion@users.noreply.github.com>
2020-07-19 16:19:07 -0400

    patch 2.0.2

--- start server.bat
+++ start server.bat
@@ -1,2 +1,2 @@
-java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar -Dlog4j.configurationFile=log4j2.xml forge-1.15.2-31.2.31.jar nogui
+java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.15.2-31.2.31.jar nogui
 PAUSE
