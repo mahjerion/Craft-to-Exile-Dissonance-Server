52a480f28fe0ce5433adba3a2273a09653b29ff0
mahjerion <66142256+mahjerion@users.noreply.github.com>
2021-01-02 23:09:52 -0500

    reduce invasion rate and update nether backport

--- start server.bat
+++ start server.bat
@@ -1,2 +1,2 @@
-java -XX:+UseG1GC -Xmx8192m -Xms256m -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.15.2-31.2.47.jar nogui
+java -XX:+UseG1GC -Xmx4096m -Xms256m -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.15.2-31.2.47.jar nogui
 PAUSE
